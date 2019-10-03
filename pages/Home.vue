d0c3312239779bbc292afa951a7962527e2d2836
Yutaka Omido <yutaka.omido@gmail.com>
2019-10-03 22:52:47 +0900

    add Home page for test

---
+++ pages/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <view>
+    <text>Click button to navigate</text>
+    <button title="Login" @press="() => navigate('Login')" />
+    <button title="MyPage" @press="() => navigate('MyPage')" />
+    <button title="UserSetting" @press="() => navigate('UserSetting')" />
+    <button title="WorldActivity" @press="() => navigate('WorldActivity')" />
+  </view>  
+</template>
+
+<script>
+export default {
+  props: {
+    navigation: {
+      type: Object
+    }
+  },
+  methods: {
+    navigate(pageName) {
+      this.navigation.navigate(pageName)
+    },
+    test() {
+      this.navigation.navigate('Login')
+    }
+  }
+}
+</script>
