ab32dfa30b6c3ee1558c8664f1b331494d7a313a
koike daiki <daiki.koike@ulsystems.co.jp>
2019-10-03 23:03:55 +0900

    mypageを作成

---
+++ components/ImagePicker.vue
@@ -0,0 +1,28 @@
+<template>
+  <view>
+    <button
+      :on-press="pickImage"
+      title="Cam"
+    >
+  </view>
+</template>
+
+<script>
+import * as ImagePicker from 'expo-image-picker';
+export default {
+    methods: {
+          pickImage: function() {
+            ImagePicker.launchCameraAsync();
+          }
+ }
+}
+</script>
+
+<style>
+.container {
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
--- pages/MyPage.vue
+++ pages/MyPage.vue
@@ -1,21 +1,67 @@
 <template>
   <view>
-    <text>MyPage</text>
-    <button title="WorldActivity" @press="onWorldActivity" />
+    <view class="timeline">
+    </view>
+    <view class="action">
+    <button
+      v-if="walking"
+      :on-press="onPressWalking"
+      title="散歩開始"
+      color="#841584"
+    />
+    <button
+      v-else
+      :on-press="onPressWalking"
+      title="散歩終了"
+      color="#841584"
+    />
+    <text-input v-model="comment" />
+    <button
+      :on-press="onPressSend"
+      title="投稿"
+      color="#993099"
+      accessibility-label="Learn more about this purple button"
+    />
+    <image-picker />
+    </view>
   </view>
 </template>
 
 <script>
-export default { 
-  props: {
-    navigation: {
-      type: Object
+import ImagePicker from "../components/ImagePicker";
+export default {
+        components: { ImagePicker },
+        data: { 
+            walking: true,
+            text: '',
+            comment: '',
+        },
+        methods: {
+          onPressWalking: function() {
+            this.walking = !this.walking
+          },
+          onPressSend: function() {
+            alert(this.comment)
+        }
+      }
     }
-  },
-  methods: {
-    onWorldActivity() {
-      this.navigation.navigate('WorldActivity')
-    }
-  }
+    </script>
+
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
 }
-</script>
+.timeline {
+  background-color: white;
+}
+.action {
+  background-color: lightslategray;
+  color: aquamarine;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
