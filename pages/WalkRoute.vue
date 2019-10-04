f7561a19ffac01b873dc6136ff58b75e0740580d
masaki.yamakawa <masaki.yamakawa@ulsystems.co.jp>
2019-10-04 10:16:01 +0900

    Change Map

--- pages/WalkRoute.vue
+++ pages/WalkRoute.vue
@@ -1,21 +1,48 @@
 <template>
   <view class="container">
-    <map-view class="container" :initial-region="coordinates" />
+    <map-view class="container" :initial-region="coordinates" :region="location" />
+
+    <touchable-opacity :on-press="getLocation" >
+      <text>Get Current Location</text>
+    </touchable-opacity>
+    <text>Current Location:</text>
+    <text>{{location.latitude}}</text>
   </view>
 </template>
 
 <script>
 import MapView from "react-native-maps";
+import * as Location from "expo-location";
+
 export default {
   data: function() {
     return {
       coordinates: {
-        latitude: 12.91074,
-        longitude: 77.5996363,
+        latitude: 35.7656561,
+        longitude: 139.2269407,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421
-      }
+      },
+      location: {},
     };
+  },
+  created: async function() {
+    await Location.requestPermissionsAsync();
+    this.location = await Location.getCurrentPositionAsync({});
+    console.log("current location=" + JSON.stringify(this.location));
+  },
+  methods: {
+    getLocation: async function() {
+      await Location.requestPermissionsAsync();
+      this.location = await Location.getCurrentPositionAsync({});
+      console.log("getLocation=" + JSON.stringify(this.location));
+      // this.coordinates = {
+      //   latitude: this.location.latitude,
+      //   longitude: this.location.longitude,
+      //   latitudeDelta: this.location.latitudeDelta,
+      //   longitudeDelta: this.location.longitudeDelta,
+      // };
+    }
   },
   components: {
     MapView
